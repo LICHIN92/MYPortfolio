56c090ac094f806e8f8405f51c81a05ce792bd29
LICHIN.C <lichinchandran1432gmail.com>
2026-05-22 07:49:09 +0530

    e

--- src/FirstPage/First.jsx
+++ src/FirstPage/First.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import './first.css'
-import pic from '../assets/IMG_20230912_133100.jpg'
+import './First.css'
+// import pic from '../assets/IMG_20230912_133100.jpg'
+import pic from '../assets/IMG_20250427_110234.jpeg'
 const First = () => {
   return (
     <div className="background_container">
@@ -8,14 +9,16 @@ const First = () => {
       <div className='gridConatiner'>
         <div className='box '>
           <h1 className='animated_text'>hi,i'm Lichin Chandran</h1> <br />
-          <h5 className=''>I develop Full Stack  Web Application, using <span className='mernstack'>MERN Stack</span></h5>
+          <h5 className=''>I develop Full Stack  Web Application, using
+            <span className='mernstack'>MERN Stack</span>
+          </h5>
         </div>
         <div className='image'>
           <img src={pic} alt="" className='mypic' />
 
         </div>
       </div>
-      
+
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './navbar/Navbar.jsx'
-import First from './FirstPage/First.jsx'
 import Introduction from './INTRODUCTION/Introduction.jsx'
 import Contact from './contact/Contact.jsx'
 import Work from './work/Work.jsx'
+import First from './FirstPage/First.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
